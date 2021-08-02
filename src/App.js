dc7dc01629d14ff789c0c5748f4a27da84865d00
luokass7 <luokass07@gmail.com>
2021-08-02 21:24:31 +0530

    demo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Table from './Table'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Table />
+    </div>
+  
+  );
+}
+
+export default App;
